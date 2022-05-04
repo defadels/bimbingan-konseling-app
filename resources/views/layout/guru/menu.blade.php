70cf0ad7d841bf4acd9922851290c22ef4813437
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-05-04 16:12:59 +0700

    Prepare controller and view

--- resources/views/layout/guru/menu.blade.php
+++ resources/views/layout/guru/menu.blade.php
@@ -9,30 +9,28 @@
                             class="hide-menu">Dashboard</span></a></li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
                         href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span
-                            class="hide-menu">Forms </span></a>
+                            class="hide-menu">Bimbingan & Konseling </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="form-basic.html" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span class="hide-menu"> Form Basic
+                                    class="mdi mdi-note-outline"></i><span class="hide-menu"> Masuk
                                 </span></a></li>
                         <li class="sidebar-item"><a href="form-wizard.html" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span class="hide-menu"> Form Wizard
+                                    class="mdi mdi-note-plus"></i><span class="hide-menu"> Ditanggapi
                                 </span></a></li>
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                    href="charts.html" aria-expanded="false"><i class="mdi mdi-account-card-details"></i><span
+                        class="hide-menu">Data Guru</span></a></li>
+
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                         href="pages-buttons.html" aria-expanded="false"><i
-                            class="mdi mdi-relative-scale"></i><span class="hide-menu">Buttons</span></a>
+                            class="mdi mdi-account-multiple"></i><span class="hide-menu">Data Siswa</span></a>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                         href="pages-buttons.html" aria-expanded="false"><i
-                            class="mdi mdi-relative-scale"></i><span class="hide-menu">Buttons</span></a>
+                            class="mdi mdi-book-open-variant"></i><span class="hide-menu">Data Kelas</span></a>
                 </li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                    href="charts.html" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
-                        class="hide-menu">Charts</span></a></li>
-          
-          
-              
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
